456ebe2cd9af4e948d39c4522036a2fc766817ad
karthick004 <96881243+karthick004@users.noreply.github.com>
2025-04-29 17:31:59 +0530

    Delete eks_cluster/backend.tf

--- eks_cluster/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "eks_cluster/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "terraform-locks" # Optional but good for concurrency
-  }
-}
